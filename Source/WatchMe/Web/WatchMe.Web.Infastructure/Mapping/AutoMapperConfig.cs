5cf7c39f4af1f71d035917e9051ae7d8b7a259b9
Alexandar Ginovski <sashko.ginovski@gmail.com>
2016-02-07 00:03:43 +0200

    Added Automapper v4

---
+++ Source/WatchMe/Web/WatchMe.Web.Infastructure/Mapping/AutoMapperConfig.cs
@@ -0,0 +1,20 @@
+﻿//namespace WatchMe.Web.Infastructure.Mapping
+//{
+//    using System;
+//    using System.Collections.Generic;
+//    using System.Linq;
+//    using System.Reflection;
+
+//    using AutoMapper;
+
+//    public class AutoMapperConfig
+//    {
+//        public MapperConfiguration GetConfiguration()
+//        {
+//            var config = new MapperConfiguration(cfg => {
+
+//            });
+//        }
+//    }
+
+//}
