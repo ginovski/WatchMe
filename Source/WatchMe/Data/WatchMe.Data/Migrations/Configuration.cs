d1fdfc9c3a8c6559cb3871acf35ea5d4cd60a0f1
Alexandar Ginovski <sashko.ginovski@gmail.com>
2016-02-06 22:13:28 +0200

    Seeded actors and directors

--- Source/WatchMe/Data/WatchMe.Data/Migrations/Configuration.cs
+++ Source/WatchMe/Data/WatchMe.Data/Migrations/Configuration.cs
@@ -1,6 +1,11 @@
 namespace WatchMe.Data.Migrations
 {
+    using System;
     using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    using Common;
+    using Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<WatchMeDbContext>
     {
@@ -14,6 +19,58 @@ namespace WatchMe.Data.Migrations
 
         protected override void Seed(WatchMeDbContext context)
         {
+            this.SeedActors(context);
+            this.SeedDirectors(context);
+        }
+        
+        private void SeedActors(WatchMeDbContext context)
+        {
+            if (context.Actors.Any())
+            {
+                return;
+            }
+
+            for (int i = 0; i < 20; i++)
+            {
+                var newActor = new Actor
+                {
+                    FirstName = RandomDataGenerator.Instance.GetRandomString(3, 15),
+                    LastName = RandomDataGenerator.Instance.GetRandomString(3, 15),
+                    Rating = new Rating
+                    {
+                        Value = RandomDataGenerator.Instance.GetRandomInt(0, 5)
+                    }
+                };
+
+                context.Actors.Add(newActor);
+            }
+
+            context.SaveChanges();
+        }
+
+        private void SeedDirectors(WatchMeDbContext context)
+        {
+            if (context.Directors.Any())
+            {
+                return;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                var newDirector = new Director
+                {
+                    FirstName = RandomDataGenerator.Instance.GetRandomString(3, 15),
+                    LastName = RandomDataGenerator.Instance.GetRandomString(3, 15),
+                    Rating = new Rating
+                    {
+                        Value = RandomDataGenerator.Instance.GetRandomInt(0, 5)
+                    }
+                };
+
+                context.Directors.Add(newDirector);
+            }
+
+            context.SaveChanges();
         }
     }
 }
