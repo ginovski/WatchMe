829ac81934b80bd3e9d15f091199873fbfc9f984
Alexandar Ginovski <sashko.ginovski@gmail.com>
2016-02-13 22:16:18 +0200

    Categories page finalized

--- Source/WatchMe/Web/WatchMe.Web/Controllers/CategoriesController.cs
+++ Source/WatchMe/Web/WatchMe.Web/Controllers/CategoriesController.cs
@@ -8,16 +8,26 @@
     using System.Linq;
     using System.Web.Mvc;
     using ViewModels.Categories;
-
+    using ViewModels.Home;
     public class CategoriesController : Controller
     {
         private const int PageSize = 5;
 
         private IMoviesService moviesService;
+        private ICategoriesService categoriesService;
 
-        public CategoriesController(IMoviesService moviesService)
+        public CategoriesController(IMoviesService moviesService, ICategoriesService categoriesService)
         {
             this.moviesService = moviesService;
+            this.categoriesService = categoriesService;
+        }
+
+        [ChildActionOnly]
+        public ActionResult All()
+        {
+            var allCategories = this.categoriesService.AllCategories().To<CategoryViewModel>().ToList();
+
+            return PartialView("Partials/_SidebarAllCategories", allCategories);
         }
 
         public ActionResult Index()
@@ -30,7 +40,7 @@
             var count = this.moviesService.MoviesInCategoryCount(id);
             ViewBag.Count = (count / PageSize) + 1;
             ViewBag.Page = page;
-            ViewBag.CategoryIdentifier = id;
+            TempData["CategoryIdentifier"] = id;
 
             var moviesInCategory = this.moviesService.MoviesInCategory(id, page - 1, PageSize).To<CategoryMovieViewModel>().ToList();
             return View(moviesInCategory);
