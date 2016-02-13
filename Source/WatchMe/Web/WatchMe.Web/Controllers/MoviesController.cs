59daaeca3add38fefe8ffdaf9b127efa8e2a8ad5
Alexandar Ginovski <sashko.ginovski@gmail.com>
2016-02-13 23:31:05 +0200

    Designed movie page for public

---
+++ Source/WatchMe/Web/WatchMe.Web/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿namespace WatchMe.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using WatchMe.Services.Data.Contracts;
+
+    public class MoviesController : Controller
+    {
+        private IMoviesService moviesService;
+
+        public MoviesController(IMoviesService moviesService)
+        {
+            this.moviesService = moviesService;
+        }
+
+        public ActionResult Details(string id)
+        {
+            return View();
+        }
+    }
+}
