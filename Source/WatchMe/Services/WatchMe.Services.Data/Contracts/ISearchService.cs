285476c7eacf6be685e46a0d6c6f82f8cb0aaf68
Alexandar Ginovski <sashko.ginovski@gmail.com>
2016-02-21 01:08:48 +0200

    Finalized syncronious search and added default actor images

---
+++ Source/WatchMe/Services/WatchMe.Services.Data/Contracts/ISearchService.cs
@@ -0,0 +1,13 @@
+﻿namespace WatchMe.Services.Data.Contracts
+{
+    using System.Linq;
+    using WatchMe.Data.Models;
+    using WatchMe.Services.Common;
+
+    public interface ISearchService : IService
+    {
+        IQueryable<Movie> Movies(string query);
+
+        IQueryable<Actor> Actors(string query);
+    }
+}
