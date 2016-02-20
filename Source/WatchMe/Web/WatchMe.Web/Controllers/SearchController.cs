26e291af0383376cc4b2c322f5dbf1531085c1c9
Alexandar Ginovski <sashko.ginovski@gmail.com>
2016-02-21 00:04:58 +0200

    Styled input for search form

---
+++ Source/WatchMe/Web/WatchMe.Web/Controllers/SearchController.cs
@@ -0,0 +1,13 @@
+﻿namespace WatchMe.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class SearchController : Controller
+    {
+        public ActionResult Index(string query)
+        {
+
+            return View();
+        }
+    }
+}
