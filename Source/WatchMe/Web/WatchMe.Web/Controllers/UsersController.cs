5837c1e8c25b11cbea14fa74a2e64db55bf9b1c9
Alexandar Ginovski <sashko.ginovski@gmail.com>
2016-02-21 20:13:57 +0200

    Updated database

---
+++ Source/WatchMe/Web/WatchMe.Web/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿namespace WatchMe.Web.Controllers
+{
+    using Microsoft.AspNet.Identity;
+    using Services.Data.Contracts;
+    using System.Web.Mvc;
+    using WatchMe.Web.Controllers.Base;
+
+    public class UsersController : BaseController
+    {
+        private IUsersService usersService;
+
+        public UsersController(IUsersService usersService)
+        {
+            this.usersService = usersService;
+        }
+
+        public ActionResult WishList()
+        {
+
+            return View();
+        }
+    }
+}
