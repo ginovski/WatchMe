9a67d9b04dbd5502805c5b5015525d0770be0bab
Alexandar Ginovski <sashko.ginovski@gmail.com>
2016-02-22 22:46:42 +0200

    Administrate movies and actors

---
+++ Source/WatchMe/Web/WatchMe.Web/Areas/Administration/Controllers/MoviesController.cs
@@ -0,0 +1,61 @@
+﻿namespace WatchMe.Web.Areas.Administration.Controllers
+{
+    using Infastructure.Mapping;
+    using Kendo.Mvc.Extensions;
+    using Kendo.Mvc.UI;
+    using Services.Data.Contracts;
+    using System.Web.Mvc;
+    using ViewModels.Movies;
+    using WatchMe.Web.Areas.Administration.Controllers.Base;
+
+    public class MoviesController : BaseAdministrationController
+    {
+        private IMoviesService moviesService;
+
+        public MoviesController(IMoviesService moviesService)
+        {
+            this.moviesService = moviesService;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Read([DataSourceRequest]DataSourceRequest request)
+        {
+            var posts = this.moviesService
+                .AllMovies()
+                .To<MovieViewModel>()
+                .ToDataSourceResult(request);
+
+            return this.Json(posts);
+        }
+
+        [HttpPost]
+        public ActionResult Destroy([DataSourceRequest]DataSourceRequest request, MovieViewModel model)
+        {
+            this.moviesService.Delete(model.Id);
+
+            return this.Json(new[] { model }.ToDataSourceResult(request, this.ModelState));
+        }
+
+        [HttpPost]
+        public ActionResult Update([DataSourceRequest]DataSourceRequest request, MovieViewModel model)
+        {
+            if (model != null && this.ModelState.IsValid)
+            {
+                var movie = this.moviesService.GetById(model.Id);
+                movie.Title = model.Title;
+                movie.Overview = model.Overview;
+                movie.Duration = model.Duration;
+                movie.ReleaseDate = model.ReleaseDate;
+
+                this.moviesService.Update(movie);
+            }
+
+            return this.Json(new[] { model }.ToDataSourceResult(request, this.ModelState));
+        }
+    }
+}
---
+++ Source/WatchMe/Web/WatchMe.Web/Areas/Administration/ViewModels/Movies/MovieViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace WatchMe.Web.Areas.Administration.ViewModels.Movies
+{
+    using System;
+    using Data.Models;
+    using Infastructure.Mapping;
+    using System.ComponentModel.DataAnnotations;
+    public class MovieViewModel : IMapFrom<Movie>
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+    
+        public int? Duration { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? ReleaseDate { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Overview { get; set; }
+    }
+}
