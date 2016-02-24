3b5c3ea19402d78bf8e8da53b8b8fb8296c4dd77
Alexandar Ginovski <sashko.ginovski@gmail.com>
2016-02-24 09:31:53 +0200

    Changed version

--- Source/WatchMe/Common/WatchMe.Web.Common/WebConstants.cs
+++ Source/WatchMe/Common/WatchMe.Web.Common/WebConstants.cs
@@ -9,7 +9,7 @@
         public const string ActorsImagesPath = "/Images/Actors";
         public const string DirectorsImagesPath = "/Images/Directors";
 
-        public const double Version = 0.2;
+        public const double Version = 0.3;
 
         public const string DefaultActorImage = ActorsImagesPath + "/default.png";
     }
