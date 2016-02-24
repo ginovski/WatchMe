1f3e7978413df5ab80e3ef11373574232b78b9d8
Alexandar Ginovski <sashko.ginovski@gmail.com>
2016-02-24 09:42:52 +0200

    Services test project added

---
+++ Source/WatchMe/Tests/WatchMe.Services.Data.Tests/RatingsServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WatchMe.Services.Data.Tests
+{
+    [TestClass]
+    public class RatingsServiceTests
+    {
+    }
+}
